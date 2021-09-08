af2d54f868aea4f7cea06e1a2cac9ce3ef1f7f59
brianpompey <brianthescholar@gmail.com>
2021-09-08 18:03:59 -0400

    added mpre

--- react-gun/src/App.js
+++ react-gun/src/App.js
@@ -23,6 +23,20 @@ export default function App() {
     name: '', message: ''
   })
 
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    const messages = gun.get('messages')
+    messages.map().on(m => {
+      dispatch({
+        name: m.name,
+        message: m.message,
+        createdAt: m.createdAt
+      })
+    })
+  }, [])
+
+
   function onChange(e) {
     setForm({ ...formState, [e.target.name]: e.target.value  })
   }
